052415bb2546779ac1c7d0ca0487e10cebb26233
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-02-15 20:09:01 +0000

    key take aways

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.shot_efficiency import linear_reg
 #page config
 st.set_page_config(page_title="GSW Dashboard", layout="wide")
 
-st.title("GSW 2022 Analysis")
+st.title("GSW 2022 Performance & Shot Efficiency Dashboard")
 
 st.markdown(
     """
@@ -27,7 +27,7 @@ with top_col1:
     st.markdown("The Golden State Warriors performed better than the NBA average in both home and away games. This is particularly important in a playoff context, as winning a series without home-court advantage requires at least one away win. Historically, in seasons where the Warriors have struggled in the playoffs, their road performance has been noticeably weaker.")
 
 
-st.subheader("Home vs Away Shot Efficieny for GSW")
+st.subheader("Home vs Away Shot Efficiency for GSW")
 scatter_col1, scatter_col2 = st.columns(2)
 with scatter_col1:
     st.plotly_chart(shot_efficiency()[0], use_container_width=True)
@@ -39,7 +39,7 @@ st.markdown(
     f"""
 The scatter plots compare the Warriors’ field goal percentage to their opponents’ field goal percentage, separated by home and away games. Points below the diagonal line indicate games where GSW shot more efficiently than their opponent, while points above indicate the opposite.
 
-From the data, when the Warriors shoot more efficiently than their opponent, they win approximately {shot_efficiency()[2]}% of home games and {shot_efficiency()[4]}% of away games. In contrast, when they shoot less efficiently, their win rate drops to around {shot_efficiency()[3]}% at home and {shot_efficiency()[5]}% away.
+The data shows that when the Warriors shoot more efficiently than their opponent, they win approximately {shot_efficiency()[2]}% of home games and {shot_efficiency()[4]}% of away games. In contrast, when they shoot less efficiently, their win rate drops to around {shot_efficiency()[3]}% at home and {shot_efficiency()[5]}% away.
 
 This suggests that relative shooting efficiency is a key determinant of game outcomes. Therefore, to better understand what drives winning performance, the next step is to analyse the factors that contribute to shot efficiency."""
 )
@@ -49,7 +49,7 @@ st.subheader("The determinants of shot efficiency")
 
 st.markdown(
     """
-A natural next step is to look at the correlations of the the covarites with shot effiency"""
+A natural next step is to examine the correlations between key covariates and shot efficiency"""
 )
 
 corr_col1, corr_col2 = st.columns(2)
